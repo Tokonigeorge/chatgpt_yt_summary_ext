012023f3ac4c56225af7b390fad9751eeebc15d9
Tokonigeorge <bukolaebikefe@yahoo.com>
2023-03-14 00:24:46 +0100

    updated manfest file

--- background.js
+++ background.js
@@ -1,10 +1,8 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    console.log(tab, tab.url)
-    if (tab.url && tab.url.includes('youtube.come/watch')) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab.url && tab.url.includes('youtube.com/watch')) {
         const queryParams = tab.url.split('?')[1]
         const urlParams = new URLSearchParams(queryParams);
 
-        console.log(urlParams, 'paramss')
         //send a message to the content script
         chromw.tabs.sendMessage(tabId, {
             type: 'NEW',
