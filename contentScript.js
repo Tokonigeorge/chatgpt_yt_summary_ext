f46ca458e2ddc3185cca7576e362e8d383820f17
Tokonigeorge <bukolaebikefe@yahoo.com>
2023-03-20 18:09:34 +0100

    fetch transcript

--- contentScript.js
+++ contentScript.js
@@ -31,7 +31,7 @@
             ytTopRow?.appendChild(summaryBtn)
         
              summaryBtn.addEventListener('click', (e) => {
-                console.log('I clicked')
+                getTranscriptLang()
             })
         }
 
@@ -47,6 +47,68 @@
         
     }
 
+    const getTranscriptLang = async () => {
+        const mainLang = "English";
+        //fetch page html 
+        const ytpage = await fetch("https://www.youtube.com/watch?v=" + currentVideo);
+        const pageHtml = await ytpage.text()
+        const splitHtml = pageHtml.split('"captions":')
+
+        //if no captions found
+        if (splitHtml.length < 2) {
+            //todo: put something for no transcript avaliable
+            return;
+        }
+
+        const captions = splitHtml[1].split(',"videoDetails')[0].replace('\n', '')
+        const captionTracks = JSON.parse(captions).playerCaptionsTracklistRenderer.captionTracks
+        const languages = captionTracks?.map(i => { return i.name.simpleText; })
+
+        //todo: take out first sort
+        languages.sort((x,y)=>{ return x.includes(mainLang) ? -1 : y.includes(mainLang) ? 1 : 0; });
+        languages.sort((x,y)=>{ return x == mainLang? -1 : y == mainLang? 1 : 0; });
+
+        const langOptions = languages?.map((name, index) => {
+            return {
+            language: name,
+            link: captionTracks.find(i => i.name.simpleText === name).baseUrl
+            }
+        })
+        
+        await getTranscript(langOptions?.filter(lang => lang.name === mainLang)?.link)
+
+    }
+
+    async function getTranscript(link) {
+        const rawTranscript = await getRawTranscript(link);
+        return rawTranscript?.map((item) => { return item.text; })?.join(' ');
+    }
+
+    async function getRawTranscript(link) {
+    // Get Transcript
+    const tsPage = await fetch(link); 
+    const tsXml= await tsPage.text();
+
+    // Parse Transcript
+    const parseTs = parseHTML(tsXml);
+    const tsNodes = parseTs.childNodes[1].childNodes;
+
+    const timeStampedTranscipt =  tsNodes?.map(i => {
+        return {
+        start: i.getAttribute("start"),
+        duration: i.getAttribute("dur"),
+        text: i.textContent
+        };
+    });
+        
+    return timeStampedTranscipt
+    }
+
+    function parseHTML(html) {
+    var t = document.createElement('template');
+        t.innerHTML = html;
+    return t.content;
+    }
 
 }
 )();
