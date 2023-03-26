4d5f3343f3531dd81ad409695a0a70e61d72c290
Tokonigeorge <bukolaebikefe@yahoo.com>
2023-03-26 01:53:44 +0100

    get summary and divide transcript into chunks

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,7 @@
 (() => {
     let ytTopRow = '';  let ytSecondary = ''
     let currentVideo = ''
+      let finalSummary = ''
 
     //listen to the message sent by the background script
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
@@ -38,8 +39,12 @@
         if (!summarySectionExists) {
             const summarySection = document.createElement('div')
             const summaryText = document.createTextNode("Summarize");
+            const summary = document.createElement('p')
+
             summarySection.className = 'summary-section'
             summarySection.appendChild(summaryText)
+            summarySection.appendChild(summary)
+            summary.innerText = finalSummary
 
             ytSecondary = document.querySelector('#secondary')
             ytSecondary?.prepend(summarySection)
@@ -75,40 +80,107 @@
             }
         })
         
-        await getTranscript(langOptions?.filter(lang => lang.name === mainLang)?.link)
-
+        await getTranscript(langOptions?.filter(lang => lang.language === mainLang)[0].link)
     }
 
-    async function getTranscript(link) {
+    const getTranscript = async (link) => {
         const rawTranscript = await getRawTranscript(link);
-        return rawTranscript?.map((item) => { return item.text; })?.join(' ');
+        const p = rawTranscript?.map((item) => { return item.text; })?.join(' ').replace(/[\n\r\s\t]+/g, ' ')
+        //if video length/transcript length  is greater than 25 mins/20000, double the chunk size
+        let chunk_size
+        if (p.length > 20000) chunk_size = 4000
+        else chunk_size = 4000
+        const arr = chunkSubstr(p, chunk_size)
+        let summary = []
+        for (let i = 0; i < arr.length; i++ ) {
+            prompt = i === 0 ? "Summarize this:"+arr[i] : "Summarize this in addition to the previous summary prompt" + arr[i]
+          const turn =  await getSummary(prompt, function(res) {
+              summary.push(res)
+              return res
+          })
+            console.log(turn)
+        }
+        console.log(summary)
+       let joined = summary.join(',')
+    //    console.log( summary)
+        if (summary.length > 1) {
+           finalSummary = getSummary(joined)
+        }
+        else {
+            finalSummary = summary[0]
+        }
+       console.log(finalSummary)
+        return finalSummary
+        
     }
-
-    async function getRawTranscript(link) {
+    const getRawTranscript = async (link) => {
     // Get Transcript
-    const tsPage = await fetch(link); 
-    const tsXml= await tsPage.text();
+    const tsPage = await fetch(link);
+
+    const tsXml = await tsPage.text().then(r=>{return r})
 
     // Parse Transcript
     const parseTs = parseHTML(tsXml);
     const tsNodes = parseTs.childNodes[1].childNodes;
-
-    const timeStampedTranscipt =  tsNodes?.map(i => {
+    const timeStampedTranscipt =  Array.from(tsNodes).map(i => {
         return {
         start: i.getAttribute("start"),
         duration: i.getAttribute("dur"),
         text: i.textContent
         };
     });
-        
     return timeStampedTranscipt
     }
 
-    function parseHTML(html) {
+    const parseHTML=(html)=> {
     var t = document.createElement('template');
         t.innerHTML = html;
     return t.content;
     }
+    const OPENAI_API_KEY = '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'
+    
+    const getSummary = async (prompt, fc) => {
+     var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://api.openai.com/v1/completions");
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("Authorization", "Bearer " + OPENAI_API_KEY)
+         var data = {
+        model: "text-davinci-003",
+        prompt: "Summarize the following text:" + prompt,
+        max_tokens: 2048,
+        // user: "1",
+        temperature:  0.5,
+        frequency_penalty: 0.0, 
+        presence_penalty: 0.0,  
+        // stop: ["#", ";"]      
+    }
+        const body = JSON.stringify(
+ data
+        );
+         let res = ''
+        xhr.onload = () => {
+   
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                res += JSON.parse(xhr.responseText).choices[0].text
+                fc(res)
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
+        xhr.send(body);
+    }
+    
+    function chunkSubstr(str, size) {
+  const numChunks = Math.ceil(str.length / size)
+  const chunks = new Array(numChunks)
+
+  for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
+    chunks[i] = str.substr(o, size)
+  }
+
+  return chunks
+}
 
 }
 )();
